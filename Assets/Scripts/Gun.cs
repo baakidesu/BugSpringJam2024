1766e0295e97c5fd60536d201990486edf2e817e
Burak <burak.foto@bahcesehir.edu.tr>
2024-04-20 23:09:09 +0300

    Black Hole
    little addons

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,25 +4,40 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
-{ 
-    
+{
     public Animator animator;
-    
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
     public static float bulletSpeed = 10f;
 
+    public float cooldownTime = 0.5f; // Atışlar arasındaki bekleme süresi
+    private bool canShoot = true; // Atış yapılabilir durum
+
     void Update()
     {
-        
-        if(Input.GetMouseButtonDown(0))
+        // Eğer ateş tuşuna basıldıysa ve atış yapılabilir durumdaysa
+        if (Input.GetMouseButtonDown(0) && canShoot)
         {
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
-            animator.SetTrigger("Shoot");
-            
+            // Ateş et
+            Shoot();
+            // Bekleme süresini başlat
+            StartCoroutine(Cooldown());
         }
-       
     }
 
+    void Shoot()
+    {
+        // Mermiyi oluştur ve ateş et
+        var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+        animator.SetTrigger("Shoot");
+    }
+
+    IEnumerator Cooldown()
+    {
+        // Atış yapılabilir durumu false yap ve bekleyip tekrar true yap
+        canShoot = false;
+        yield return new WaitForSeconds(cooldownTime);
+        canShoot = true;
+    }
 }
